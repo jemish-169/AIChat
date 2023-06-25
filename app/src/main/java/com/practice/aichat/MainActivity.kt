b83f617c8385dc9c38cb054a4d608f097336f3cd
jemish-169 <jemishkhunt169@gmail.com>
2023-06-25 16:59:30 +0530

    removed jetpack compose code and built with xml

---
+++ app/src/main/java/com/practice/aichat/MainActivity.kt
@@ -0,0 +1,122 @@
+package com.Practice.AIChat
+
+import android.annotation.SuppressLint
+import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.EditText
+import android.widget.ImageButton
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.easychatgpt.R
+import okhttp3.*
+import okhttp3.MediaType.Companion.toMediaType
+import okio.IOException
+import org.json.JSONException
+import org.json.JSONObject
+
+
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var recyclerView: RecyclerView
+    private lateinit var welcomeTextView: TextView
+    private lateinit var messageEditText: EditText
+    private lateinit var sendButton: ImageButton
+    private var messageList: MutableList<Message> = ArrayList()
+    private lateinit var messageAdapter: MessageAdapter
+
+    private val JSON = "application/json; charset=utf-8".toMediaType()
+    private val client = OkHttpClient()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        messageList = ArrayList()
+
+        recyclerView = findViewById(R.id.recycler_view)
+        welcomeTextView = findViewById(R.id.welcome_text)
+        messageEditText = findViewById(R.id.message_edit_text)
+        sendButton = findViewById(R.id.send_btn)
+
+        // Setup recycler view
+        messageAdapter = MessageAdapter(messageList)
+        recyclerView.adapter = messageAdapter
+        val llm = LinearLayoutManager(this)
+        llm.stackFromEnd = true
+        recyclerView.layoutManager = llm
+
+        sendButton.setOnClickListener {
+            val question = messageEditText.text.toString().trim()
+            addToChat(question, Message.SENT_BY_ME)
+            messageEditText.setText("")
+            callAPI(question)
+            welcomeTextView.visibility = View.GONE
+        }
+    }
+
+    @SuppressLint("NotifyDataSetChanged")
+    private fun addToChat(message: String, sentBy: String) {
+        runOnUiThread {
+            messageList.add(Message(message, sentBy))
+            messageAdapter.notifyDataSetChanged()
+            recyclerView.smoothScrollToPosition(messageAdapter.itemCount)
+        }
+    }
+
+    private fun addResponse(response: String) {
+        messageList.removeAt(messageList.size - 1)
+        addToChat(response, Message.SENT_BY_BOT)
+    }
+
+    private fun callAPI(question: String) {
+        // okhttp
+        messageList.add(Message("Typing... ", Message.SENT_BY_BOT))
+
+        val jsonBody = JSONObject()
+        try {
+            jsonBody.put("model", "text-davinci-003")
+            jsonBody.put("prompt", question)
+            jsonBody.put("max_tokens", 4000)
+            jsonBody.put("temperature", 0)
+        } catch (e: JSONException) {
+            e.printStackTrace()
+        }
+        val body = RequestBody.create(JSON, jsonBody.toString())
+        val request = Request.Builder()
+            .url("https://api.openai.com/v1/completions")
+            .header("Authorization", "Bearer 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")
+            .post(body)
+            .build()
+
+        client.newCall(request).enqueue(object : Callback {
+            override fun onFailure(call: Call, e: IOException) {
+                addResponse("Failed to load response due to " + e.message)
+                e.message?.let { Log.d("my-message", it) }
+            }
+
+            @Throws(IOException::class)
+            override fun onResponse(call: Call, response: Response) {
+                Log.d("response", response.toString())
+                if (response.isSuccessful) {
+                    var jsonObject: JSONObject? = null
+                    try {
+                        jsonObject = response.body?.string()?.let { JSONObject(it) }
+                        val jsonArray = jsonObject?.getJSONArray("choices")
+                        val result = jsonArray?.getJSONObject(0)?.getString("text")
+                        if (result != null) {
+                            addResponse(result.trim())
+                        }
+                        response.body?.let { Log.d("my-message", it.toString()) }
+                    } catch (e: JSONException) {
+                        e.printStackTrace()
+                    }
+                } else {
+                    addResponse("Failed to load response due to " + response.body!!.toString())
+                    response.body?.let { Log.d("my-message", "holaa" + it.toString()) }
+                }
+            }
+        })
+    }
+}
--- app/src/main/java/com/practice/aichat/utils/Resource.kt
+++
@@ -1,7 +0,0 @@
-package com.practice.AIChat.utils
-
-sealed class Resource<T> {
-    data class Success<T>(val data: T) : Resource<T>()
-
-    class Error<T>(val exception: Exception) : Resource<T>()
-}
